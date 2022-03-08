67d0580170dbd9c6937e6dffe94a95fce2528ee7
stat1c <fynn.weyrich@web.de>
2022-03-08 10:57:59 +0100

    setup tests

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export type { OpineRequest, OpineResponse, NextFunction } from "https://deno.land/x/opine@2.1.2/mod.ts";
 export { spy } from "https://deno.land/x/mock@0.13.0/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
--- mod_test.ts
+++ mod_test.ts
@@ -1,8 +1,5 @@
-/*
-import { test, assertEquals } from "./test_deps.ts";
-import { getHelloWorld } from "./mod.ts";
+import { assertEquals } from "./test_deps.ts";
 
-test(function test_get_hello_world() {
-  assertEquals(getHelloWorld(), "\x1b[1mHello World\x1b[22m");
+Deno.test("assert true", () => {
+  assertEquals(true, true);
 });
-*/
