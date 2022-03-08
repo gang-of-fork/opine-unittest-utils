2925fc67ba7a5e02d21fe8fd5cb00ab6097ee331
stat1c <fynn.weyrich@web.de>
2022-03-08 11:06:41 +0100

    fix dependency

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
 // Add your test dependencies in here
-export { assertEquals } from "https://deno.land/std@0.32.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
